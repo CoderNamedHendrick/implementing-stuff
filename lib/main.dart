479c8b5039b7c1f694f8fb2644fd97f150cb848d
Hendrick <sebastinesoacatp@gmail.com>
2021-07-10 22:53:23 +0100

    modal implementation -- bottom modal implementation

--- lib/main.dart
+++ lib/main.dart
@@ -41,7 +41,9 @@ class HomePage extends StatelessWidget {
                 child: Text("Simple Dialog Modal"),
               ),
               RaisedButton(
-                onPressed: () {},
+                onPressed: () {
+                  _showBottomModal(context);
+                },
                 child: Text("Bottom Modal"),
               ),
               RaisedButton(
@@ -84,6 +86,104 @@ class HomePage extends StatelessWidget {
                   ),
                 ],
               ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  _showBottomModal(context) {
+    return showModalBottomSheet(
+      context: context,
+      backgroundColor: Colors.transparent,
+      builder: (builder) {
+        return new Container(
+          color: Colors.transparent,
+          child: new Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(10.0),
+                topRight: Radius.circular(10.0),
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black12,
+                  blurRadius: 10.0,
+                  spreadRadius: 0.0,
+                ),
+              ],
+            ),
+            alignment: Alignment.topLeft,
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(top: 5, left: 10),
+                      child: Text(
+                        "Bottom Modal",
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w900,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(top: 5, right: 5),
+                      child: FlatButton(
+                        padding: EdgeInsets.fromLTRB(5, 0, 5, 0),
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: Text(
+                          "Save",
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xff999999),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 5),
+                Container(
+                  padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+                  decoration: BoxDecoration(
+                    border: Border(
+                      top: BorderSide(
+                        color: Color(0xfff8f8f8),
+                        width: 1,
+                      ),
+                    ),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      RichText(
+                        textAlign: TextAlign.justify,
+                        text: TextSpan(
+                          text: "Bottom Modal Text",
+                          style: TextStyle(
+                            fontWeight: FontWeight.w400,
+                            fontSize: 14,
+                            color: Colors.black,
+                            wordSpacing: 1,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
           ),
         );

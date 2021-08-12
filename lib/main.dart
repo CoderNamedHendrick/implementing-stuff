f287bb0d080a08c5909c642a58b80f467ba31066
Hendrick <sebastinesoacatp@gmail.com>
2021-08-12 23:29:34 +0100

    spring animation with physics

--- lib/main.dart
+++ lib/main.dart
@@ -1,60 +1,112 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/physics.dart';
 
 void main() {
   runApp(const MaterialApp(
-    home: Page1(),
+    home: PhysicsCardDragDemo(),
   ));
 }
 
-class Page1 extends StatelessWidget {
-  const Page1({Key key}) : super(key: key);
+class PhysicsCardDragDemo extends StatelessWidget {
+  const PhysicsCardDragDemo({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () => Navigator.of(context).push(_createRoute()),
-          child: const Text('Go!'),
+      body: const DraggableCard(
+        child: FlutterLogo(
+          size: 128,
         ),
       ),
     );
   }
-
-  Route _createRoute() {
-    return PageRouteBuilder(
-      pageBuilder: (context, animation, secondaryAnimation) => const Page2(),
-      transitionsBuilder: (context, animation, secondaryAnimation, child) {
-        var curve = Curves.ease;
-        var revCurve = Curves.easeOutCubic;
-        const begin = Offset(1.0, 0.0);
-        const end = Offset.zero;
-        // final tween =
-        //     Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
-        // final offsetAnimation = animation.drive(tween);
-
-        final tween = Tween(begin: begin, end: end);
-        final curvedAnimation = CurvedAnimation(
-            parent: animation, curve: curve, reverseCurve: revCurve);
-        return SlideTransition(
-          position: tween.animate(curvedAnimation),
-          child: child,
-        );
-      },
-    );
-  }
 }
 
-class Page2 extends StatelessWidget {
-  const Page2({Key key}) : super(key: key);
+class DraggableCard extends StatefulWidget {
+  const DraggableCard({Key key, @required this.child}) : super(key: key);
+  final Widget child;
+
+  @override
+  _DraggableCardState createState() => _DraggableCardState();
+}
+
+class _DraggableCardState extends State<DraggableCard>
+    with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+  Animation<Alignment> _animation;
+  Alignment _dragAlignment = Alignment.center;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    )..addListener(
+        () {
+          setState(() {
+            _dragAlignment = _animation.value;
+          });
+        },
+      );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _runAnimation(Offset pixelsPerSecond, Size size) {
+    _animation = _controller.drive(
+      AlignmentTween(
+        begin: _dragAlignment,
+        end: Alignment.center,
+      ),
+    );
+
+    // Calculate the velocity relative to the unit interval, [0,1]
+    // used by the animation controller.
+    final unitsPerSecondX = pixelsPerSecond.dx / size.width;
+    final unitsPerSecondY = pixelsPerSecond.dy / size.height;
+    final unitsPerSecond = Offset(unitsPerSecondX, unitsPerSecondY);
+    final unitVelocity = unitsPerSecond.distance;
+
+    const spring = SpringDescription(
+      mass: 30,
+      stiffness: 1,
+      damping: 1,
+    );
+
+    final simulation = SpringSimulation(spring, 0, 1, -unitVelocity);
+
+    _controller.animateWith(simulation);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: const Center(
-        child: Text('Page 2'),
+    var size = MediaQuery.of(context).size;
+    return GestureDetector(
+      onPanDown: (details) {
+        _controller.stop();
+      },
+      onPanUpdate: (details) {
+        setState(() {
+          _dragAlignment += Alignment(
+            details.delta.dx / (size.width / 2),
+            details.delta.dy / (size.height / 2),
+          );
+        });
+      },
+      onPanEnd: (details) {
+        _runAnimation(details.velocity.pixelsPerSecond, size);
+      },
+      child: Align(
+        alignment: _dragAlignment,
+        child: Card(
+          child: widget.child,
+        ),
       ),
     );
   }

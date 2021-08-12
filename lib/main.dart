cf7067f76d835f09643278d13d0a5c10571a9f73
Hendrick <sebastinesoacatp@gmail.com>
2021-08-12 09:50:21 +0100

    non-animated page transition

--- lib/main.dart
+++ lib/main.dart
@@ -1,106 +1,46 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(LogoApp());
+  runApp(const MaterialApp(
+    home: Page1(),
+  ));
 }
 
-class LogoApp extends StatefulWidget {
-  const LogoApp({Key key}) : super(key: key);
-
-  @override
-  _LogoAppState createState() => _LogoAppState();
-}
-
-class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
-  Animation<double> animation;
-  AnimationController controller;
-
-  @override
-  void initState() {
-    super.initState();
-    controller =
-        AnimationController(vsync: this, duration: const Duration(seconds: 2));
-    animation = CurvedAnimation(parent: controller, curve: Curves.easeIn)
-      ..addStatusListener((status) {
-        if (status == AnimationStatus.completed) {
-          controller.reverse();
-        } else if (status == AnimationStatus.dismissed) {
-          controller.forward();
-        }
-      });
-    controller.forward();
-  }
-
-  @override
-  Widget build(BuildContext context) => AnimatedLogo(animation: animation);
-
-  @override
-  void dispose() {
-    controller.dispose();
-    super.dispose();
-  }
-}
-
-class AnimatedLogo extends AnimatedWidget {
-  const AnimatedLogo({
-    Key key,
-    @required Animation<double> animation,
-  }) : super(key: key, listenable: animation);
-
-  static final _opacityTween = Tween<double>(begin: 0.1, end: 1);
-  static final _sizeTween = Tween<double>(begin: 0, end: 300);
+class Page1 extends StatelessWidget {
+  const Page1({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final animation = listenable as Animation<double>;
-    return Center(
-      child: Opacity(
-        opacity: _opacityTween.evaluate(animation),
-        child: Container(
-          margin: const EdgeInsets.symmetric(vertical: 10),
-          height: _sizeTween.evaluate(animation),
-          width: _sizeTween.evaluate(animation),
-          child: const FlutterLogo(),
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () => Navigator.of(context).push(_createRoute()),
+          child: const Text('Go!'),
         ),
       ),
     );
   }
-}
 
-class GrowTransition extends StatelessWidget {
-  const GrowTransition(
-      {Key key, @required this.child, @required this.animation})
-      : super(key: key);
-
-  final Widget child;
-  final Animation<double> animation;
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: AnimatedBuilder(
-        animation: animation,
-        builder: (context, child) {
-          return SizedBox(
-            height: animation.value,
-            width: animation.value,
-            child: child,
-          );
-        },
-        child: child,
-      ),
+  Route _createRoute() {
+    return PageRouteBuilder(
+      pageBuilder: (context, animation, secondaryAnimation) => const Page2(),
+      transitionsBuilder: (context, animation, secondaryAnimation, child) =>
+          child,
     );
   }
 }
 
-class LogoWidget extends StatelessWidget {
-  const LogoWidget({Key key}) : super(key: key);
+class Page2 extends StatelessWidget {
+  const Page2({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.symmetric(vertical: 10),
-      child: const FlutterLogo(),
+    return Scaffold(
+      appBar: AppBar(),
+      body: const Center(
+        child: Text('Page 2'),
+      ),
     );
   }
 }

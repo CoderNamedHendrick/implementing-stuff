0d6d30cd3fc062a78d7c7784f1d2670bc3a19fdd
Hendrick <sebastinesoacatp@gmail.com>
2021-08-12 23:47:57 +0100

    animated container🥺🥺

--- lib/main.dart
+++ lib/main.dart
@@ -1,112 +1,65 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
-import 'package:flutter/physics.dart';
 
 void main() {
   runApp(const MaterialApp(
-    home: PhysicsCardDragDemo(),
+    home: AnimatedContainerApp(),
   ));
 }
 
-class PhysicsCardDragDemo extends StatelessWidget {
-  const PhysicsCardDragDemo({Key key}) : super(key: key);
+class AnimatedContainerApp extends StatefulWidget {
+  const AnimatedContainerApp({Key key}) : super(key: key);
+
+  @override
+  _AnimatedContainerAppState createState() => _AnimatedContainerAppState();
+}
+
+class _AnimatedContainerAppState extends State<AnimatedContainerApp> {
+  double _width = 50;
+  double _height = 50;
+  Color _color = Colors.green;
+  BorderRadiusGeometry _borderRadius = BorderRadius.circular(8);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: const DraggableCard(
-        child: FlutterLogo(
-          size: 128,
+      body: Center(
+        child: AnimatedContainer(
+          width: _width,
+          height: _height,
+          decoration: BoxDecoration(
+            color: _color,
+            borderRadius: _borderRadius,
+          ),
+          duration: const Duration(seconds: 1),
+          curve: Curves.fastOutSlowIn,
         ),
       ),
-    );
-  }
-}
-
-class DraggableCard extends StatefulWidget {
-  const DraggableCard({Key key, @required this.child}) : super(key: key);
-  final Widget child;
-
-  @override
-  _DraggableCardState createState() => _DraggableCardState();
-}
-
-class _DraggableCardState extends State<DraggableCard>
-    with SingleTickerProviderStateMixin {
-  AnimationController _controller;
-  Animation<Alignment> _animation;
-  Alignment _dragAlignment = Alignment.center;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = AnimationController(
-      vsync: this,
-      duration: const Duration(seconds: 1),
-    )..addListener(
-        () {
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
           setState(() {
-            _dragAlignment = _animation.value;
+            final random = Random();
+
+            // Generate a random width and height.
+            _width = random.nextInt(300).toDouble();
+            _height = random.nextInt(300).toDouble();
+
+            // Generate a random color
+            _color = Color.fromRGBO(
+              random.nextInt(256),
+              random.nextInt(256),
+              random.nextInt(256),
+              1,
+            );
+
+            // Generate a random border radius
+            _borderRadius =
+                BorderRadius.circular(random.nextInt(100).toDouble());
           });
         },
-      );
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  void _runAnimation(Offset pixelsPerSecond, Size size) {
-    _animation = _controller.drive(
-      AlignmentTween(
-        begin: _dragAlignment,
-        end: Alignment.center,
-      ),
-    );
-
-    // Calculate the velocity relative to the unit interval, [0,1]
-    // used by the animation controller.
-    final unitsPerSecondX = pixelsPerSecond.dx / size.width;
-    final unitsPerSecondY = pixelsPerSecond.dy / size.height;
-    final unitsPerSecond = Offset(unitsPerSecondX, unitsPerSecondY);
-    final unitVelocity = unitsPerSecond.distance;
-
-    const spring = SpringDescription(
-      mass: 30,
-      stiffness: 1,
-      damping: 1,
-    );
-
-    final simulation = SpringSimulation(spring, 0, 1, -unitVelocity);
-
-    _controller.animateWith(simulation);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    var size = MediaQuery.of(context).size;
-    return GestureDetector(
-      onPanDown: (details) {
-        _controller.stop();
-      },
-      onPanUpdate: (details) {
-        setState(() {
-          _dragAlignment += Alignment(
-            details.delta.dx / (size.width / 2),
-            details.delta.dy / (size.height / 2),
-          );
-        });
-      },
-      onPanEnd: (details) {
-        _runAnimation(details.velocity.pixelsPerSecond, size);
-      },
-      child: Align(
-        alignment: _dragAlignment,
-        child: Card(
-          child: widget.child,
-        ),
+        child: const Icon(Icons.play_arrow),
       ),
     );
   }
